1a091a7a117ee74071ce87f7d59d2489a001f4a4
keenanhammonds <keenan.hammonds@gmail.com>
2019-08-19 16:05:34 -0400

    setting us up for greatness

--- basketball-react-app/src/App.js
+++ basketball-react-app/src/App.js
@@ -1,28 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Search from "./Components/Search"
-import Home from "./Components/Home"
+import Search from "./Components/Search";
 import Comparison from "./Components/Comparison"
+import Home from "./Components/Home"
+import {BrowserRouter as Route, Link} from 'react-router-dom'
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-       <header className="headerContainer">
-        <h1>App</h1>
-      </header>
-      <Search className="searchContainer"/>
-      <main>
-        <Route path="/" exact component={Home} />
-        <Route path="/compare" component={Comparison} />
-      </main>
-      <footer className="footerContainer">
-
-      </footer>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="headerContainer">
+          <h1>App</h1>
+        </header>
+        <Search className="searchContainer" />
+        <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/compare" component={Comparison} />
+        </main>
+        <footer className="footerContainer" />
+      </div>
+    );
+  }
 }
 
 export default App;
