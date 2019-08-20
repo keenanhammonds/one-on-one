d90e09edae90d9517d861bd9492ad1f6ae85c8fa
keenanhammonds <keenan.hammonds@gmail.com>
2019-08-20 15:12:26 -0400

    passing the player data through props from app js to comparison component

--- basketball-react-app/src/Components/Comparison.js
+++ basketball-react-app/src/Components/Comparison.js
@@ -3,6 +3,9 @@ import Card from "./Card"
 
 class Comparison extends Component {
   render() {
+    //save the player data to variable
+    const playerData = this.props.players;
+    console.log(playerData)
     return (
       <div className="cardsContainer">
         <Card />
