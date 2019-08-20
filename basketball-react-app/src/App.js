cf466e47767699a47d1dcd61f8e883cd2c7a2a1c
Susej_Roffe <susejroffe@gmail.com>
2019-08-20 14:32:04 -0400

    video

--- basketball-react-app/src/App.js
+++ basketball-react-app/src/App.js
@@ -6,36 +6,25 @@ import Home from "./Components/Home"
 import {Route, Link} from 'react-router-dom'
 import Card from "./Components/Card"
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "./logoNBA.png";
 
 class App extends Component {
   render() {
     return (
-      
       <div className="App">
-        
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <a class="navbar-brand"></a>
-       <img src={logo} width="100" height="80"/>
-       <div class="search">
-     <span class="fa fa-search"></span>
-     <input placeholder="Search for a player"/>
-     </div>
-   </nav>
-   
-   
-           {/* main middle container */}
-   <main className="mainContainer">
-       <Route path="/" exact render={routerProps => <Home />} />
-       <Route path="/compare" exact render={routerProps => <Comparison />} />
-   </main>
-      
-      {/* footer */}
-      <footer className="footerContainer">
+        <header className="headerContainer">
+          <Link to="/">
+          <h1>Header</h1>
+          </Link>
+        </header>
+        <Search className="searchContainer" />
+        <main className="mainContainer">
+          <Route path="/" exact render={routerProps => <Home />} />
+          <Route path="/compare" exact render={routerProps => <Comparison />} />
+        </main>
+        <footer className="footerContainer">
         <h1>footer</h1>
         </footer>
-        </div>
+      </div>
     );
   }
 }
--- basketball-react-app/src/Components/Home.js
+++ basketball-react-app/src/Components/Home.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
-import {Route, Link} from "react-router-dom"
+import {Route, Link} from "react-router-dom";
 
+import video from "./NBAmix2.mp4";
 
 class Home extends Component {
   render() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-        <Link to="/compare"><h1>example link to functional component</h1></Link>
-      </div>
-    );
+     return (
+      <video className='videoTag' autoPlay loop muted>
+      <source src={video} type='video/mp4' />
+      </video>
+        
+      );
+    }
   }
-}
 
 export default Home;
