45f6964170e05ca3292f4b3c8028945e40825a19
Susej_Roffe <susejroffe@gmail.com>
2019-08-19 21:57:00 -0400

    setup

--- basketball-react-app/src/App.js
+++ basketball-react-app/src/App.js
@@ -3,23 +3,34 @@ import axios from "axios";
 import Search from "./Components/Search";
 import Comparison from "./Components/Comparison"
 import Home from "./Components/Home"
-import {BrowserRouter as Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logoNBA.png";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="headerContainer">
-          <h1>App</h1>
-        </header>
-        <Search className="searchContainer" />
-        <main>
-          <Route path="/" exact component={Home} />
-          <Route path="/compare" component={Comparison} />
-        </main>
-        <footer className="footerContainer" />
+      <Router>
+      <div className="container">
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="https://google.com">
+        <img src={logo} width="100" height="50"/>
+        </a>
+        <Link to="/" className="navbar-brand">App</Link>
+      
+      <div className="collapse nav-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/compare" className="nav-link">OnexOne</Link>
+          </li>
+        </ul>
+
       </div>
+      </nav>
+      </div>
+      </Router>
     );
   }
 }
