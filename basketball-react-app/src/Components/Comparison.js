59aa44642b67339bf31320f9873daa8ec2d68309
Victor Johnson <vjohnson22@gmail.com>
2019-08-21 16:18:07 -0400

    made player match functionality work

--- basketball-react-app/src/Components/Comparison.js
+++ basketball-react-app/src/Components/Comparison.js
@@ -1,77 +1,22 @@
 import React, { Component } from "react";
-import Card from "./Card"
-import axios from "axios"
 import './Comparison.css'
 import PlayerMatch from './PlayerMatch'
 
- let player1  
-//  {
-//   "_id": "5d5c59b595824f00049e5ef2",
-//   "name": "Wilt Chamberlain",
-//   "ppg": 30.1,
-//   "apg": 4.4,
-//   "rpg": 22.9,
-//   "spg": null,
-//   "bpg": null,
-//   "startYear": 1959,
-//   "endYear": 1972,
-//   "__v": 0
-// }
- let player2 
-//  = {
-//   "_id": "5d5c3d55ceccb90004dfd3f5",
-//   "name": "LeBron James",
-//   "ppg": 27.2,
-//   "apg": 7.2,
-//   "rpg": 7.4,
-//   "spg": 1.6,
-//   "bpg": 0.8,
-//   "startYear": 2003,
-//   "endYear": 2018,
-//   "__v": 0
-// }
+ 
 
 class Comparison extends Component {
-  constructor() {
-    super();
-
-    this.state= {
-      
-      player1:[],
-      player2:[]
-    }
-  }
-  componentDidMount(){
-    
-  // get yearavgs
-  axios.get(`https://basketball-era.herokuapp.com/yearavg/yearavg/${player1.startYear}/${player1.endYear}`)   
-    .then( res => {
-      this.setState({player1: res.data})
-    })
-   axios.get(`https://basketball-era.herokuapp.com/yearavg/yearavg/${player2.startYear}/${player2.endYear}`)   
-    .then( res => {
-      this.setState({player2:  res.data})
-    })  
-
- 
-  }
+  
+  
   render() {
-    if(this.props.matchup.length === 0){
-      player1 = []
-      player2 = []
-    }else if(this.props.matchup.length === 1){
-      player1 = this.props.matchup[0].player
-      player2 = []
-    } else if(this.props.matchup.length === 2){
-      player1 = this.props.matchup[0].player
-      player2 = this.props.matchup[1].player
-    } 
-    console.log(this.props.matchup.length)
+    let playerMatch = this.props.matchup.map( (player, i) => {
+      return <PlayerMatch key={i} player ={player.player}/>
+    })
+     
+    
     return (
       <div>
       <div className="cardsContainer">
-        <PlayerMatch {...this.props.matchup[0]} player = {player1} data ={this.state.player1}/>
-        <PlayerMatch {...this.props.matchup[1]} player = {player2} data ={this.state.player2}/>
+            {playerMatch}
         </div>
         
       </div>
--- basketball-react-app/src/Components/PlayerMatch.js
+++ basketball-react-app/src/Components/PlayerMatch.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react'
 import Card from './Card'
 import Match from './Match'
+import axios from 'axios'
 
 class PlayerMatch extends Component {
+    constructor(){
+        super()
 
+        this.state= {
+            data: []
+        }
+    }
+    componentDidMount(){
+        axios.get(`https://basketball-era.herokuapp.com/yearavg/yearavg/${this.props.player.startYear}/${this.props.player.endYear}`)   
+        .then( res => {
+           
+          this.setState({data: res.data})
+        })
+         
+    }
     render(){
         return(
             <div>
                 <Card/>
-                <Match player = {this.props.player} data = {this.props.data}/>
+                <Match player = {this.props.player} data = {this.state.data}/>
             </div>
         )
     }
