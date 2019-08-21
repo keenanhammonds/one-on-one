e0ad65ea652ae5fbb615feff5e53ab1b8299033d
Susej_Roffe <susejroffe@gmail.com>
2019-08-21 09:54:45 -0400

    reactjs

--- basketball-react-app/src/Components/Card.js
+++ basketball-react-app/src/Components/Card.js
@@ -4,9 +4,12 @@ class Card extends Component {
 
   render() {
     return (
-      <div className="card">
-        <h1>Player Image</h1>
+      <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="..." alt="Card image cap" />
+      <div className="card-body">
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
+    </div>
     );
   }
 }
