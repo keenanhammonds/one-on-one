e3d98179f61ab356e88a9c28e73195c55c229f27
keenanhammonds <keenan.hammonds@gmail.com>
2019-08-22 15:21:17 -0400

    test

--- basketball-react-app/src/index.js
+++ basketball-react-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import "tachyons"
-import WebFont from 'webfontloader';
+import "tachyons";
+import WebFont from "webfontloader";
 
 WebFont.load({
   google: {
-    families: ['Titillium Web:300,400,700', 'sans-serif']
+    families: ["Titillium Web:300,400,700", "sans-serif"]
   }
 });
 
