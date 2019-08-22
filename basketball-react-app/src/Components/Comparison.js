7f8178bbc73a3e69e93daa3e721eefe55ce8e045
keenanhammonds <keenan.hammonds@gmail.com>
2019-08-22 08:27:18 -0400

    added functioning reset button, added react tilt when more than one player is selected it automatically places new player in matchup,

--- basketball-react-app/src/Components/Comparison.js
+++ basketball-react-app/src/Components/Comparison.js
@@ -1,24 +1,21 @@
 import React, { Component } from "react";
-import './Comparison.css'
-import PlayerMatch from './PlayerMatch'
-
- 
+import "./Comparison.css";
+import PlayerMatch from "./PlayerMatch";
 
 class Comparison extends Component {
-  
-  
   render() {
-    let playerMatch = this.props.matchup.map( (player, i) => {
-      return <PlayerMatch key={i} player ={player.player}/>
-    })
-     
-    
+    let playerMatch = this.props.matchup.map((player, i) => {
+      return(
+         <PlayerMatch key={i} player={player.player} />
+         );
+    });
+
     return (
-      <div className="containerContainer">
-      <div className="cardsContainer">
-            {playerMatch}
+      <div>
+        <div>
+          <button onClick={this.props.handleReset}>reset</button>
         </div>
-        
+        <div className="cardsContainer">{playerMatch}</div>
       </div>
     );
   }
