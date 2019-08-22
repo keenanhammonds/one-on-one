7c0ba417e1870ff5087d3079e7660b708dbd9bc7
keenanhammonds <keenan.hammonds@gmail.com>
2019-08-22 14:16:19 -0400

    fixed merge conflicts

--- basketball-react-app/src/App.js
+++ basketball-react-app/src/App.js
@@ -2,16 +2,12 @@ import React, { Component } from "react";
 import axios from "axios";
 import Search from "./Components/Search";
 import Comparison from "./Components/Comparison";
-import Home from "./Components/home/Home";
+import Home from "./Components/Home/Home";
 import { Route, Link } from "react-router-dom";
 import "./App.css";
-import Create from './Components/Create'
-import Delete from './Components/Delete'
-<<<<<<< HEAD
-import WebFont from 'webfontloader';
-=======
-import Update from './Components/Update'
->>>>>>> e7a679f6eecd33360053b1a8a2746adc4d1cfad2
+import Create from "./Components/Create";
+import Delete from "./Components/Delete";
+import WebFont from "webfontloader";
 
 class App extends Component {
   constructor() {
@@ -22,35 +18,28 @@ class App extends Component {
     };
   }
 
-
-
   handleClick = (player, evt) => {
     evt.preventDefault();
+    console.log("clicked", player);
     if (this.state.matchup.length < 2) {
       this.setState({ matchup: [...this.state.matchup, { player }] });
-    } 
+    }
     if (this.state.matchup.length === 2) {
-      const arr =  [...this.state.matchup, { player }]
-      const newArr = arr.pop()
+      const arr = [...this.state.matchup, { player }];
+      const newArr = arr.pop();
       this.setState({ matchup: [newArr] });
     }
-<<<<<<< HEAD
 
     console.log(this.state);
-=======
-    
-    
->>>>>>> e7a679f6eecd33360053b1a8a2746adc4d1cfad2
   };
 
-
-  handleReset = (evt) => {
+  handleReset = evt => {
     evt.preventDefault();
     this.setState({
       matchup: []
-    })
-  }
-  
+    });
+  };
+
   getData = () => {
     axios.get("https://basketball-era.herokuapp.com").then(res => {
       this.setState({
@@ -81,17 +70,26 @@ class App extends Component {
           <Route
             path="/compare"
             exact
-            render={routerProps => <Comparison handleReset={this.handleReset} matchup={this.state.matchup} players={this.state.players} />}
+            render={routerProps => (
+              <Comparison
+                handleReset={this.handleReset}
+                matchup={this.state.matchup}
+                players={this.state.players}
+              />
+            )}
           />
-          <Route path="/create" exact render= {() => <Create/>}/>
-          <Route path="/delete" exact render= {() => <Delete/>}/>
-          <Route path="/update" exact render= {() => <Update/>}/>
+          <Route path="/create" exact render={() => <Create />} />
+          <Route path="/delete" exact render={() => <Delete />} />
         </main>
         <footer className="footerContainer">
-        <Link to= "compare"> <h3>Compare Players</h3></Link>
-        <Link to='/create'> <h3>Add Player</h3></Link>
-        <Link to='/update'> <h3>Update Player</h3></Link>
-        <Link to='/delete'> <h3>Delete Player</h3></Link>
+          <Link to="/create">
+            {" "}
+            <h3>Add Player</h3>
+          </Link>
+          <Link to="/delete">
+            {" "}
+            <h3>Delete Player</h3>
+          </Link>
         </footer>
       </div>
     );
