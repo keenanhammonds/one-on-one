5f434229dbf94531a2dfeb8243474603ec4e8477
Victor Johnson <vjohnson22@gmail.com>
2019-08-23 04:58:51 -0400

    chart normalized

--- basketball-react-app/src/Components/Chart.js
+++ basketball-react-app/src/Components/Chart.js
@@ -14,36 +14,59 @@ class Chart extends Component {
                   label: "Player",
                   type: "bar",
                   borderColor: "rgba(255,0,0,0.2)",
-                  backgroundColor: "rgba(255,0,0,0.2)",
+                  backgroundColor: "rgba(255,0,0,0.5)",
                   data: this.props.playerData,
                   fill: false
                 }, 
                 {
                   label: "Era",
                   type: "bar",
-                  backgroundColor: "rgba(0,255,0,0.2)",
+                  backgroundColor: "rgba(0,255,0,0.5)",
                   data: this.props.eraData,
                 }, {
                   label: "% Difference",
                   type: "line",
-                  backgroundColor: "rgba(0,0,255, 0.2)",
+                  backgroundColor: "rgba(0,0,255, 0.5)",
                   backgroundColorHover: "rgba(255,255,255,.2)",
                   data: this.props.difference
                 }
               ]
             }
-            // options: {
-            //   title: {
-            //     display: true,
-            //     text: 'Population growth (millions): Europe & Africa'
-            //   },
-            //   legend: { display: false }
-            // }
+            const chartOptions = {
+              responsive: true,
+              legend: {
+                  position: 'bottom',
+              },
+              hover: {
+                  mode: 'label'
+              },
+              scales: {
+                  xAxes: [{
+                          display: true,
+                         
+                      }],
+                  yAxes: [{
+                          display: true,
+                          scaleLabel: {
+                            display: true,
+                            labelString: '% Dif from Era Top 10'
+                        },
+                          ticks: {
+                              beginAtZero: true,
+                              steps: 10,
+                              stepValue: 5,
+                              max: 100,
+                              min: -100
+                          }
+                      }]
+              },
+             
+          }
     
 
         return(
             <div>
-            <Bar data={ChartData}/>
+            <Bar data={ChartData} options={chartOptions}/>
             </div>
         )
     }
